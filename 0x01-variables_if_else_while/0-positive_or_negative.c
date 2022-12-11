a05e1d096dbcb88d343272e11cc77caadc7bf4d6
kalfredcollins <kalfredcollins@gmail.com>
2022-12-11 06:27:59 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,13 @@
-#include <stdio.h>                                                                                                                    
-#include <stdlib.h>                                                                                                                   
-#include <time.h>                                                                                                                     
-                                                                                                                                      
-                                                                                                                                      
-/**                                                                                                                                   
- *  * main - Entry point for the block                                                                                                   
- *   * Return : Always 0                                                                                                                  
- *    *                                                                                                                                    
- *     */                                                                                                                                   
-                                                                                                                                      
-int main(void)                                                                                                                                    
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: Always 0 for success
+ */
+int main(void)
 {
-	int n;                                                                                                                        
-		                                                                                                                                      
-		        srand(time(0))                                                                                                                
-				        n = rand() - RAND_MAX / 2;                                                                                                    
-			        if (n > 0)                                                                                                                    
-					                printf("%i is positive\n", n);                                                                                        
-				        else if (n < 0)                                                                                                               
-						                printf("%i is negative\n", n);                                                                                        
-					        else                                                                                                                          
-							                printf("% is zero\n", n);                                                                                             
-						        return (0); 
+	int n;
+
+	srand(time(0));
+	n = rand
